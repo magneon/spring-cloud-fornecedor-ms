57ae7def76d400b0395679557bccbc2a816ff559
Rafael de Mesquita Moura <rafael.mesquita@softcube.com.br>
2020-03-20 11:26:17 -0300

    Commit Inicial do Projeto

---
+++ src/main/java/br/com/softcube/microservices/fornecedor/FornecedorMsApplication.java
@@ -0,0 +1,13 @@
+package br.com.softcube.microservices.fornecedor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornecedorMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornecedorMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/softcube/microservices/fornecedor/FornecedorMsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.softcube.microservices.fornecedor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornecedorMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
